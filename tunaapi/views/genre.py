5b9d50c08e2c07627165bbdec636875a6ec20049
GrahamJReid <grahamreidtechnology@gmail.com>
2023-06-06 09:52:50 -0500

    crud Genre functional

---
+++ tunaapi/views/genre.py
@@ -0,0 +1,77 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from tunaapi.models import Genre
+
+
+
+class GenreView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
+        Returns:
+            Response -- JSON serialized game type
+        """
+        try:
+            genre = Genre.objects.get(pk=pk)
+            serializer = GenreSerializer(genre)
+            return Response(serializer.data)
+        except Genre.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+          
+    def list(self, request):
+        """Handle GET requests to get all game types
+
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        genre = Genre.objects.all()
+        serializer = GenreSerializer(genre, many=True)
+        return Response(serializer.data)
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized game instance
+        """
+        
+        genre = Genre.objects.create(
+            description=request.data["description"],
+            
+            
+        )
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
+      
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        genre = Genre.objects.get(pk=pk)
+        genre.description = request.data["description"]
+        
+      
+        genre.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+      
+    def destroy(self, request, pk):
+        """Delete Artists
+        """
+        genre = Genre.objects.get(pk=pk)
+        genre.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)      
+class GenreSerializer(serializers.ModelSerializer):
+    """JSON serializer for events
+    """
+    class Meta:
+        model = Genre
+        fields = ('id', 'description')
+        depth = 1
